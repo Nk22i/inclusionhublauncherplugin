a267ad76d0f1fb574b4884f3956a15973fa2ebdd
Nicolas Kovalsky <kovalsky.nicolas@gmail.com>
2014-12-01 13:50:46 -0300

    saco corchete

--- www/launcherPlugin.js
+++ www/launcherPlugin.js
@@ -6,9 +6,9 @@ var launcherPlugin = {
             errorCallback, // error callback function
             'LauncherPlugin', // mapped to our native Java class called "Calendar"
             'getInstalledApps', // with this action name
-            [{                  // and this array of custom arguments to create our entry
+            [                // and this array of custom arguments to create our entry
             	"!"
-            }]
+            ]
         ); 
 	}
 }
